45dd6edae9603dcc9a3ab21391c2e8444fce36de
Yoonjae Jung <yoonjae01072501510@gmail.com>
2026-09-02 13:34:27 +0900

    Delete Source directory

--- Source/AladinProject/AladinProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AladinProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AladinProject, "AladinProject" );
